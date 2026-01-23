5dc846e01594a3cc72a12326911574a646f95a08
RookieLoveCoding <1622708386@qq.com>
2026-01-23 16:44:32 +0800

    [2026.01.23 liu]完善了一下文档

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -99,7 +99,7 @@ int main(void)
     /* USER CODE BEGIN 3 */
     /* Blink LED: toggle LED_R and wait 500 ms */
     HAL_GPIO_TogglePin(LED_R_GPIO_Port, LED_R_Pin);
-    HAL_Delay(100);
+    HAL_Delay(600);
   }
   /* USER CODE END 3 */
 }
